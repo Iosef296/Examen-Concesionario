039dce181bb94636978eb78ff382bdac458eae64
Iosef296 <imma210809@gmail.com>
2024-11-11 06:29:11 -0500

    Add files via upload

---
+++ ConcesionariaIosef.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ConcesionariaIosef {
+    Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+
+    }
+    public void marcas(){}
+    public void postventa(){}
+    public void cotizar(){}
+    public void financiamiento(){}
+    public void VendeTuAuto(){}
+    public void talleres(){}
+    public void seminuevos(){}
+    public void coorporativo(){}
+    public void AgendarCitaenTaller(){}
+    public void nosotros(){}
+    public void promociones(){}
+}
+
